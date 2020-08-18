f221b6f96944f4cfb72b2aca63d0a51a8b11384f
thb <2339068373@qq.com>
2020-08-18 19:20:29 +0800

    点击登录，将菜单打印出来

--- src/test/java/cn/edu/guet/SpringTest.java
+++ src/test/java/cn/edu/guet/SpringTest.java
@@ -1,5 +1,7 @@
 package cn.edu.guet;
 
+import cn.edu.guet.model.Menu;
+import cn.edu.guet.service.IUserService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
